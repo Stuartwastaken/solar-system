e6777232904cacfd1ed74a47a9fad1b80d699700
Stuartwastaken <88628337+Stuartwastaken@users.noreply.github.com>
2025-02-02 21:52:04 -0600

    Lagrange Points added

--- src/components/SolarSystem.tsx
+++ src/components/SolarSystem.tsx
@@ -2,13 +2,15 @@ import React from 'react';
 import Sun from './Sun';
 import Planet from './Planet';
 import GravityGrid from './GravityGrid';
+import LagrangeVertices from './LagrangeVertices';
 import { BODIES } from './CelestialBodies';
 
 interface SolarSystemProps {
   timeScale: number;
+  showLagrangePoints: boolean;
 }
 
-const SolarSystem: React.FC<SolarSystemProps> = ({ timeScale }) => {
+const SolarSystem: React.FC<SolarSystemProps> = ({ timeScale, showLagrangePoints }) => {
   return (
     <>
       <Sun />
@@ -16,6 +18,7 @@ const SolarSystem: React.FC<SolarSystemProps> = ({ timeScale }) => {
         <Planet key={body.name} body={body} timeScale={timeScale} />
       ))}
       <GravityGrid timeScale={timeScale} />
+      {showLagrangePoints && <LagrangeVertices timeScale={timeScale} visible={showLagrangePoints} />}
     </>
   );
 };
